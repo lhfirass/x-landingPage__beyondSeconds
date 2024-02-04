488b18006f6b587ba82bc7233358ca58671de431
lhfirass <81319618+lhfirass@users.noreply.github.com>
2024-02-04 17:14:44 +0100

    commit: 04/02/2024 : 17:14
    + HTML

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="">
+      <div className="">
+        <div className="icon"></div>
+        <div className="lg-hero-text">
+          <p>THE eWatch with the newest microchip technology</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam.</p>
+        </div>
+        <div className="btns">
+          <a href="">Order now</a>
+          <a href="">Explore more</a>
+        </div>
+        <div className="more">
+          <div className="open-time">
+
+          </div>
+          <div className="stars">
+
+          </div>
+          <div className="views">
+
+          </div>
+          <div className="webs">
+            
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      {/* */}
+      
+      <div className="hero-img">
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
